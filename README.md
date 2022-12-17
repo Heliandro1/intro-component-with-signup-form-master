ed530d9564f595dd4327e0dc90f49c291d8d7f71
ACC <aderitocaxala10@gmail.com>
2022-12-17 17:07:24 +0100

    ending the project

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# intro-component-with-signup-form-master
- 
+# Frontend Mentor - Intro component with sign up form solution
+
+This is a solution to the [Intro component with sign up form challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/intro-component-with-signup-form-5cf91bd49edda32581d28fd1). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+  - [Built with](#built-with)
+  - [Author](#author)
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Receive an error message when the `form` is submitted if:
+  - Any `input` field is empty. The message for this error should say *"[Field Name] cannot be empty"*
+  - The email address is not formatted correctly (i.e. a correct email address should have this structure: `name@host.tld`). The message for this error should say *"Looks like this is not an email"*
+
+### Screenshot
+
+![](./images/screenshot.png)
+
+### Links
+
+- Solution URL: [here](https://github.com/Heliandro1/intro-component-with-signup-form-master)
+- Live Site URL: [here](https://signup-form-master-psi.vercel.app/)
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+
+## Author
+
+- Frontend Mentor - [@heliandro1](https://www.frontendmentor.io/profile/heliandro1)
+- Linkedin - [@adérito-caxala](https://www.linkedin.com/in/adérito-caxala)
+- Instagram - [@heliandro_10](https://www.instagram.com/heliandro_10)
+- Facebook - [@Heliandro](https://www.facebook.com/heliandro.1)
--- script/index.js
+++ script/index.js
@@ -1,7 +1,6 @@
 const btnSend = document.querySelector("#btnSend");
 const errorInputs = document.querySelectorAll("input[data-error]");
 btnSend.addEventListener("click", isValidFields);
-
 function isValidFields(e) {
     e.preventDefault();
     const fieldFirstName = document.querySelector("#firstName");
@@ -10,24 +9,24 @@ function isValidFields(e) {
     const fieldPassword = document.querySelector("#password");
     if (fieldFirstName.value.trim() == '') {
         setError('First Name cannot be empty', '#firstName');
-    }else if(fieldName.dataset.erro == 'error'){
+    }else if(fieldFirstName.dataset.error == 'error'){
         removeError('#firstName');
     }
     if (fieldLastName.value.trim() == '') {
         setError('Last Name cannot be empty', '#lastName');
-    }else if(fieldName.dataset.erro == 'error'){
+    }else if(fieldLastName.dataset.error == 'error'){
         removeError('#lastName');
     }
     if (fieldEmail.value.trim() == '') {
-        setError('This field is required', '#email');
+        setError('Looks like this is not an email', '#email');
     }else if(!isValidEmail(fieldEmail.value.trim())){
-        setError('Invalid Email Adress', '#email');
-    }else if(fieldEmail.dataset.erro == 'error'){
+        setError('Looks like this is not an email', '#email');
+    }else if(fieldEmail.dataset.error == 'error'){
         removeError('#email');
     }
     if (fieldPassword.value.trim() == '') {
         setError('Password cannot be empty', '#password');
-    }else if(fieldPassword.dataset.erro == 'error'){
+    }else if(fieldPassword.dataset.error == 'error'){
         removeError('#password');
     }
 }
@@ -35,25 +34,32 @@ function isValidFields(e) {
 function setError(error, field) {
     const errorIcon = document.querySelector(`div > ${field} + span`);
     const errorElement = document.querySelector(`div > ${field}`);
-    const errorField = document.querySelector(`${errorElement.parentElement.parentElement.tagName}`);
     const p = document.createElement("p");
     p.classList.add("error");
     p.innerText = error;
     errorElement.style.borderColor = 'hsl(0, 100%, 74%)';
     errorElement.dataset.error = 'error';
     errorIcon.style.display = 'block';
-    if (errorField.children.length == 2) {
-        errorField.removeChild(errorField.lastChild);
-        errorField.appendChild(p)
+    if (errorElement.parentElement.parentElement.children.length == 2) {
+        errorElement.parentElement.parentElement.removeChild(errorElement.parentElement.parentElement.lastChild);
+        errorElement.parentElement.parentElement.appendChild(p)
     }else{
-        errorField.appendChild(p);
+        errorElement.parentElement.parentElement.appendChild(p);
     }
 }
 function removeError(field) {
-    const errorField = document.querySelector(field);
-    const errorElement = document.querySelector(`${field} + input`);
-    errorElement.style.borderColor = 'hsl(229, 24%, 87%)';
-    if (errorField.children.length == 2) {
-        errorField.removeChild(errorField.lastChild);
+    const errorElement = document.querySelector(field);
+    const errorIcon = document.querySelector(`${field} + span`);
+    errorElement.style.borderColor = 'hsl(246, 25%, 77%)';
+    if (errorElement.parentElement.parentElement.children.length == 2) {
+        errorElement.parentElement.parentElement.removeChild(errorElement.parentElement.parentElement.lastChild);
+        errorIcon.style.display = 'none';
     }
+}
+function isValidEmail(email) {
+    let regex = /^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?(?:\.[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?)*$/gi
+    if (regex.test(email)) {
+        return true;
+    }
+    return false;
 }
